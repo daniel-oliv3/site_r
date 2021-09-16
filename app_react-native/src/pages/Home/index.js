57645f47d71180684fc40ab0c7ac5946ce57f472
Daniel Oliveira <danielsapup3@gmail.com>
2021-09-16 18:18:48 -0300

    Update recebendo dados da api

--- app_react-native/src/pages/Home/index.js
+++ app_react-native/src/pages/Home/index.js
@@ -1,8 +1,44 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, {useCallback, useState} from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import { Alert, Text } from 'react-native';
+
+import api from '../../config/configApi';
+import { FlatList } from 'react-native-gesture-handler';
 
 export default function Home(){
+
+    const [orcamentos, setOrcamentos] = useState('');
+
+    const getOrcamento = async () => {
+        try {
+            const response = await api.get('/listar-orcamento');
+            setOrcamentos(response.data.orcamento);
+            console.log(response.data.orcamento);
+        }catch(err){
+            Alert.alert("", "Erro: Nenhum orçamento encontrado! tente mais tarde!")
+        }
+    }
+
+
+    useFocusEffect(
+        useCallback(() => {
+            getOrcamento();
+        },[])
+    )
+
     return (
-        <Text>Home - Bem Vindo!</Text>
+        <>
+            <Text>Listar Orçamentos</Text>
+            <FlatList
+                data={orcamentos}
+                renderItem={({ item }) => (
+                    <>
+                        <Text>{item.id}</Text>
+                        <Text>{item.name}</Text>
+                        <Text>{item.subject}</Text>
+                    </>
+                )} keyExtractor={orcamentos => String(orcamentos.id)}
+            />
+        </>
     )
 }
